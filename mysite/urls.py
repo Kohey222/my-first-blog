d7ce0f7c44dcc37002299b86f8102ca21e86d02f
kskmt0414 <45933391+kskmt0414@users.noreply.github.com>
2019-01-20 02:52:00 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('blog.urls')),
-    url('', include('social_django.urls', namespace='social')),
+    path('', include('social_django.urls', namespace='social')),
 ]
