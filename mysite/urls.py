7e155346d32bca35c6712d16506a8afda35c5bc5
kskmt0414 <45933391+kskmt0414@users.noreply.github.com>
2019-01-20 02:19:15 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('user/', include('blog.urls')),
-    url('', include('social_django.urls', namespace='social')),
+    path('admin/', admin.site.urls),
+    path('user/', include('blog.urls')),
+    path('', include('social_django.urls', namespace='social')),
 ]
